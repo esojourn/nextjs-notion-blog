a8a2bdc7d1a0f7da806b9dffeac64bfb386860a2
Ding Xuan <esojourn@gmail.com>
2024-11-22 13:17:39 +0800

    added GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -106,6 +107,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </SectionContainer>
         </ThemeProviders>
       </body>
+      <GoogleAnalytics gaId="G-T07W22H5ZF" />
     </html>
   )
 }
